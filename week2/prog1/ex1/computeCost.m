da5b723efb5ff84542143a30e648c535120c15ae
Jim Leach <jim.leach@kpmg.co.uk>
2015-07-24 10:07:14 +0100

    correctly implemented code to calculate cost function

--- week2/prog1/ex1/computeCost.m
+++ week2/prog1/ex1/computeCost.m
@@ -7,12 +7,18 @@ function J = computeCost(X, y, theta)
 m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
-J = 0;
+% J = 0;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
+const = 1/(2*m);
+hx = (X*theta);
+diff = (hx - y).^2;
+summat = sum(diff);
+
+J = const*summat;
 
 
 
